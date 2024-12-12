3282dfc2200aa25f41e91dc29be2bcb2ddf3be7f
lioneladitya30 <lioneladitya30@gmail.com>
2024-12-12 20:26:03 +0000

    mengubah tampilan login

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:myapp/app/controllers/auth_controller.dart';
 import 'package:myapp/app/routes/app_pages.dart';
@@ -8,77 +7,145 @@ import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
   final cAuth = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-            Image.network(
-              "https://static-00.iconduck.com/assets.00/android-plain-wordmark-icon-256x256-ppoejbtc.png",
-              height: 100,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: controller.cEmail,
-              decoration: InputDecoration(
-                labelText: "Email",
-              ),
-            ),
-            TextField(
-              controller: controller.cPass,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                cAuth.login(controller.cEmail.text, controller.cPass.text);
-              },
-              child: Text("Login"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 105, 150, 34),
-                foregroundColor: Colors.white,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                child: Text("Reset Password"),
-                onPressed: () {
-                  Get.offAllNamed(Routes.RESET_PASSWORD);
-                },
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade500, Colors.blue.shade200],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text("Belum Punya Akun ?"),
-                TextButton(
+                const SizedBox(height: 60),
+                // Logo Aplikasi
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.school,
+                    color: Colors.blue.shade700,
+                    size: 50,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Kelasku",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "Manajemen jadwal kuliah dengan mudah",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white70,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                // Input Email
+                TextField(
+                  controller: controller.cEmail,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Email",
+                    hintStyle: TextStyle(color: Colors.grey.shade700),
+                    prefixIcon: Icon(Icons.email, color: Colors.grey.shade700),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Input Password
+                TextField(
+                  controller: controller.cPass,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Password",
+                    hintStyle: TextStyle(color: Colors.grey.shade700),
+                    prefixIcon: Icon(Icons.lock, color: Colors.grey.shade700),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                // Tombol Login
+                ElevatedButton(
                   onPressed: () {
-                    Get.offAllNamed(Routes.SIGNUP);
+                    cAuth.login(controller.cEmail.text, controller.cPass.text);
                   },
-                  child: Text("Daftar Disini"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.blue.shade700,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 60, vertical: 15),
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Reset Password
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      Get.offAllNamed(Routes.RESET_PASSWORD);
+                    },
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Colors.white70),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                // Daftar Akun Baru
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(color: Colors.white70),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Get.offAllNamed(Routes.SIGNUP);
+                      },
+                      child: Text(
+                        "Sign up!",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
