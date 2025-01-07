d7e59c63460513cddcace0830821db882d0e929d
aldion30892 <aldion30892@gmail.com>
2025-01-07 14:55:58 +0000

    (Finishing ROUTE untuk semua page)

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:myapp/app/modules/Tugas/views/Tugas_add_view.dart';
 import '../controllers/home_controller.dart';
 import 'package:myapp/app/modules/mahasiswa/views/mahasiswa_add_view.dart';
+import 'package:myapp/app/modules/Materi/views/Materi_add_view.dart';
+import 'package:myapp/app/modules/Deadline/views/Deadline_add_view.dart';
 import 'package:myapp/app/modules/mahasiswa/views/mahasiswa_view.dart';
+import 'package:myapp/app/modules/Materi/views/Materi_view.dart';
+import 'package:myapp/app/modules/Tugas/views/Tugas_view.dart';
+import 'package:myapp/app/modules/Deadline/views/Deadline_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class HomeView extends StatelessWidget {
@@ -24,25 +30,25 @@ class _DashboardAdminState extends State<DashboardAdmin> {
     {
       'title': 'Mata Kuliah',
       'view': (BuildContext context) => MahasiswaView(type: 'mata_kuliah'),
-      'add': () => MahasiswaAddView(type: 'mata_kuliah'),
+      'add': (BuildContext context) => MahasiswaAddView(type: 'mata_kuliah'),
       'icon': Icons.school,
     },
     {
       'title': 'Daftar Tugas',
-      'view': (BuildContext context) => MahasiswaView(type: 'tugas'),
-      'add': () => MahasiswaAddView(type: 'tugas'),
+      'view': (BuildContext context) => TugasView(),
+      'add': (BuildContext context) => TugasAddView(),
       'icon': Icons.task,
     },
     {
       'title': 'Pencatatan Materi',
-      'view': (BuildContext context) => MahasiswaView(type: 'materi'),
-      'add': () => MahasiswaAddView(type: 'materi'),
+      'view': (BuildContext context) => MateriView(),
+      'add': (BuildContext context) => MateriAddView(),
       'icon': Icons.note_alt,
     },
     {
       'title': 'Daftar Deadline',
-      'view': (BuildContext context) => MahasiswaView(type: 'deadline'),
-      'add': () => MahasiswaAddView(type: 'deadline'),
+      'view': (BuildContext context) => DeadlineView(),
+      'add': (BuildContext context) => DeadlineAddView(),
       'icon': Icons.calendar_today,
     },
   ];
@@ -60,6 +66,15 @@ class _DashboardAdminState extends State<DashboardAdmin> {
           _fragment[_index]['title'],
           style: TextStyle(fontWeight: FontWeight.w600),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              // Tambahkan fungsi membuka halaman tambah
+              Get.to(_fragment[_index]['add'](context));
+            },
+          ),
+        ],
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -87,108 +102,108 @@ class _DashboardAdminState extends State<DashboardAdmin> {
   }
 
   Widget _buildDrawer() {
-  return Drawer(
-    child: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.lightBlue.shade700, Colors.lightBlue.shade400],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return Drawer(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.lightBlue.shade700, Colors.lightBlue.shade400],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.lightBlue.shade900, Colors.lightBlue.shade700],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundColor: Colors.lightBlue.shade300,
-                  child: Text(
-                    "GN",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.lightBlue.shade900, Colors.lightBlue.shade700],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-                SizedBox(width: 16),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Geels No Counter",
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.lightBlue.shade300,
+                    child: Text(
+                      "GN",
                       style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
-                    SizedBox(height: 4),
-                    Text(
-                      'Welcome!',
-                      style: TextStyle(fontSize: 14, color: Colors.white70),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _fragment.length,
-              itemBuilder: (context, i) {
-                return ListTile(
-                  onTap: () {
-                    setState(() {
-                      _index = i;
-                    });
-                    Get.back();
-                  },
-                  leading: Icon(_fragment[i]['icon'], color: Colors.white),
-                  title: Text(
-                    _fragment[i]['title'],
-                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
-                  trailing: Icon(Icons.navigate_next, color: Colors.white),
-                );
+                  SizedBox(width: 16),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Geels No Counter",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        'Welcome!',
+                        style: TextStyle(fontSize: 14, color: Colors.white70),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _fragment.length,
+                itemBuilder: (context, i) {
+                  return ListTile(
+                    onTap: () {
+                      setState(() {
+                        _index = i;
+                      });
+                      Get.back();
+                    },
+                    leading: Icon(_fragment[i]['icon'], color: Colors.white),
+                    title: Text(
+                      _fragment[i]['title'],
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    trailing: Icon(Icons.navigate_next, color: Colors.white),
+                  );
+                },
+              ),
+            ),
+            Divider(color: Colors.white54),
+            ListTile(
+              onTap: () {
+                logout(context); // Panggil fungsi logout
               },
+              leading: Icon(Icons.logout, color: Colors.redAccent),
+              title: Text(
+                'Logout',
+                style: TextStyle(color: Colors.redAccent, fontSize: 16),
+              ),
+              trailing: Icon(Icons.navigate_next, color: Colors.redAccent),
             ),
-          ),
-          Divider(color: Colors.white54),
-          ListTile(
-            onTap: () {
-              logout(context); // Panggil fungsi logout
-            },
-            leading: Icon(Icons.logout, color: Colors.redAccent),
-            title: Text(
-              'Logout',
-              style: TextStyle(color: Colors.redAccent, fontSize: 16),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
+              child: Text(
+                "Version 1.0",
+                style: TextStyle(color: Colors.white70, fontSize: 12),
+              ),
             ),
-            trailing: Icon(Icons.navigate_next, color: Colors.redAccent),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
-            child: Text(
-              "Version 1.0",
-              style: TextStyle(color: Colors.white70, fontSize: 12),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
   // Fungsi logout
   void logout(BuildContext context) async {
